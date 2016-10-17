235c61e30f2a080b972b68ffc3c967ce1f8a787e
Yin Shanyang <yang@swarm.is>
2016-10-17 23:18:16 +0800

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,16 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install cask
+brew tap caskroom/cask
+
+# install apps from brew
+
+# install apps from brew cask
+brew cask install atom
+brew cask install alfred
+brew cask install amethyst
+brew cask install google-chrome
+brew cask install slack
+brew cask install spotify
+
